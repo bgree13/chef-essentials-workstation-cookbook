65fa74ceb8c57706405c95223ddee67652757653
ChefDK User <chef@ip-10-238-167-196.ec2.internal>
2016-12-12 19:09:27 +0000

    Added unit testing

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -16,3 +16,13 @@ describe port(80) do
   it { should_not be_listening }
   skip 'This is an example test, replace with your own test.'
 end
+
+describe package('tree') do
+  it { should be_installed}
+  skip 'This is the tree package test'
+end
+
+describe file('/etc/motd') do
+  its('content') { should match(/Property of/) }	
+  it { should be_owned_by 'root' }
+end
