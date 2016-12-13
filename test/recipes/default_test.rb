01a5a50d688064ea61cd98ef76a7bf9b3b866fc1
ChefDK User <chef@ip-10-238-167-196.ec2.internal>
2016-12-13 00:53:58 +0000

    Kitchen updates

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -26,3 +26,7 @@ describe file('/etc/motd') do
   its('content') { should match(/Property of/) }	
   it { should be_owned_by 'root' }
 end
+
+describe file('/etc/motd') do
+  it { should be_mode 0777 }
+end
